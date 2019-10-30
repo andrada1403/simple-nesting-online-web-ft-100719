a91667ef64d48eaba4e9577815739cc272f69c23
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-30 20:40:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,7 @@ programmer_hash[:yukihiro_matsumoto]= {}
 programmer_hash[:yukihiro_matsumoto][:known_for]= "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages]= ["LISP"]
 programmer_hash[:yukihiro_matsumoto][:languages]<< "C"
+programmer_hash
 end
 
 def changing_alan
